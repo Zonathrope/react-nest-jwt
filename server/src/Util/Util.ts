a999c90897070164d9612628bc340cec14015a62
Oleksandr Kachanov <54705048+Zonathrope@users.noreply.github.com>
2021-08-27 22:12:07 +0300

    Add func to check passwords

--- server/src/Util/Util.ts
+++ server/src/Util/Util.ts
@@ -1,5 +1,11 @@
 import * as hash from 'password-hash'
 
+//hash a string
 export async function hashString(string: String){
     return hash.generate(string)
+}
+
+// Compares a plain-text password (password) to a hashed password (hashedPassword) and returns a boolean. Both arguments are required.
+export async function checkHashedString(normalString: String, hashedString: String){
+    return hash.verify(normalString, hashedString)
 }
